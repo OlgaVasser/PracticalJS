1175684bfb856f600f1aedb8f9b2fb3569f83dd6
OlgaVasser <olga.vasil01@gmail.com>
2021-02-07 22:06:08 +0200

    HW7

---
+++ HW7/3 уровень/1-tabs/app.js
@@ -0,0 +1,62 @@
+"use strict";
+const texts = {
+  text1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  text2:
+    "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.",
+  text3:
+    "Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.",
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+
+let linkText = document.querySelector(".text");
+let linksArray = document.querySelectorAll(".nav-link");
+linksArray.forEach((linkArray) => {
+  linkArray.addEventListener("click", clickHandler);
+});
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event
+ */
+function clickHandler(event) {
+  // здесь вызывайте changeText и changeActiveClass, и передавайте
+  // им объект события.
+  changeActiveClass(event);
+  changeText(event);
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event
+ */
+function changeActiveClass(event) {
+  for (let i = 0; i < linksArray.length; i++) {
+    linksArray[i].classList.remove("active");
+  }
+  event.target.classList.add("active");
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event
+ */
+function changeText(event) {
+  let newText = event.target.textContent;
+
+  if (newText === "Link 1") {
+    linkText.textContent = texts.text1;
+  } else if (newText === "Link 2") {
+    linkText.textContent = texts.text2;
+  } else if (newText === "Link 3") {
+    linkText.textContent = texts.text3;
+  }
+}
